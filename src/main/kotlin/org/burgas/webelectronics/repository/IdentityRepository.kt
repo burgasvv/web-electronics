ccdced05bae4a875a1bf0a450582122579a0e7ab
Burgas <burgassme@gmail.com>
2025-10-11 20:01:54 +0700

    Добавление Identity entity graph

--- src/main/kotlin/org/burgas/webelectronics/repository/IdentityRepository.kt
+++ src/main/kotlin/org/burgas/webelectronics/repository/IdentityRepository.kt
@@ -1,6 +1,7 @@
 package org.burgas.webelectronics.repository
 
 import org.burgas.webelectronics.entity.identity.Identity
+import org.springframework.data.jpa.repository.EntityGraph
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 import java.util.Optional
@@ -9,7 +10,11 @@ import java.util.UUID
 @Repository
 interface IdentityRepository : JpaRepository<Identity, UUID> {
 
+    @EntityGraph(value = "identity-entity-graph", type = EntityGraph.EntityGraphType.FETCH)
     override fun findById(id: UUID): Optional<Identity>
+
+    @EntityGraph(value = "identity-entity-graph", type = EntityGraph.EntityGraphType.FETCH)
+    override fun findAll(): List<Identity>
 
     fun findIdentityByEmail(email: String): Optional<Identity>
 }
